542ab7ddf5a93cf1cef92890784917ac5a313431
shixin060 <shixin060@163.com>
2016-01-10 01:33:58 +0800

    Initial commit

---
+++ src/com/zsx/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zsx.demo;
+
+/**
+ * Created by habit on 16/1/10.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.print("Hello World");
+    }
+}
+
